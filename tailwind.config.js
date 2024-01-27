682f9c3991a05300b024b747f71d455c9503371e
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-01-26 17:07:24 -0700

    Finish the styles of the card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Red-Hat-Display': ['Red Hat Display', 'sans-serif']
+      },
+      colors: {
+        'pale-blue' : 'hsl(225, 100%, 94%)',
+        'bright-blue' : 'hsl(245, 75%, 52%)',
+        'very-pale-blue' : 'hsl(225, 100%, 98%)',
+        'desaturated-blue' : 'hsl(224, 23%, 55%)',
+        'dark-blue': 'hsl(223, 47%, 23%)',
+        'gray-l': 'hsl(228, 100%, 99%, 100%)'
+      },
+      boxShadow:{
+        'bt': '0px 4px 4px 0px rgba(0,0,0,0.25)'
+      }
+    },
   },
   plugins: [],
 }
