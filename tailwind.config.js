0abe5b68f15c8387d7d4985c015290d09978fe21
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-01-31 17:46:02 -0700

    Add color hover the button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
       spacing: {
@@ -15,7 +15,8 @@ module.exports = {
         'very-pale-blue' : 'hsl(225, 100%, 98%)',
         'desaturated-blue' : 'hsl(224, 23%, 55%)',
         'dark-blue': 'hsl(223, 47%, 23%)',
-        'gray-l': 'hsl(228, 100%, 99%, 100%)'
+        'gray-l': 'hsl(228, 100%, 99%, 100%)',
+        'purl-hov': '#766CF1'
       },
       boxShadow:{
         'bt': '0px 4px 4px 0px rgba(0,0,0,0.25)'
